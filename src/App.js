c33952372af3a7602bce589a0741459089080359
charlyguz <gusmanc622@gmail.com>
2022-10-20 20:21:22 -0500

    termine pagina credit card

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Staking from './pages/staking/Staking.js';
 import Landing from './pages/landing/Landing.js';
 import Mint from './pages/mint/Mint.js';
+import CC from './pages/credit card/CC.js';
 
 function App() {
   return (
     <React.Fragment>
-      <Mint/>
+      <CC/>
+      {/* <Mint/> */}
       {/* <Landing/> */}
       {/* <Staking/> */}
     </React.Fragment>
